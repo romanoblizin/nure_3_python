d931d7bd991af6bf92eb087db26529da25cebd5c
turbozver <turbozver24@gmail.com>
2026-01-04 03:59:03 +0300

    =)

---
+++ examples.py
@@ -0,0 +1,57 @@
+with open("data.txt", "r") as f:
+    data = f.read()
+
+
+with open("data.txt", "w") as f:
+    f.write("example")
+
+
+with open("data.bin", "wb") as f:
+    f.write(b"\x00\x01\x02")
+
+
+class Player:
+    @property
+    def hp(self):
+        return self._hp
+    
+
+class Example:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class A:
+    pass
+
+class B:
+    pass
+
+class C(A, B):
+    pass
+
+
+def gen():
+    for i in range(3):
+        yield i
+
+
+def outer(x):
+    def inner():
+        return x
+    
+    return inner
+
+
+import unittest
+
+class TestMath(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(2 + 2, 4)
+
+unittest.main()
+
+
